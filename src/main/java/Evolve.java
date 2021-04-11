9358e17112353c5b5c9a7df46940529fdb278549
Zvika Haramaty <haramaty.zvika@gmail.com>
2021-04-11 13:38:13 +0300

    initial

---
+++ src/main/java/Evolve.java
@@ -0,0 +1,18 @@
+// run with -file first.params
+
+import ec.*;
+
+public class Evolve {
+    // based on ec.Evolve - has 'simple main' in comments:
+    public static void main(String[] args) {
+        EvolutionState state = ec.Evolve.possiblyRestoreFromCheckpoint(args);
+        if (state != null)  // loaded from checkpoint
+            state.run(EvolutionState.C_STARTED_FROM_CHECKPOINT);
+        else {
+            state = ec.Evolve.initialize(ec.Evolve.loadParameterDatabase(args), 0);
+            state.run(EvolutionState.C_STARTED_FRESH);
+        }
+        ec.Evolve.cleanup(state);
+        System.exit(0);
+    }
+}
