0fef490da4e7dfcf16b677b749162faf93abed91
Zvika Haramaty <haramaty.zvika@gmail.com>
2021-05-04 08:30:39 +0300

    grammar: basic derivation, with `cons` and `cons3`

---
+++ src/main/java/func/Cons3.java
@@ -0,0 +1,34 @@
+package func;
+
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
+
+public class Cons3 extends GPNode { // implements EvalPrint {
+    public String toString() { return "Cons3"; }
+
+    public void eval(final EvolutionState state,
+                     final int thread,
+                     final GPData input,
+                     final ADFStack stack,
+                     final GPIndividual individual,
+                     final Problem problem)
+        {
+            double result = 3;
+//        double result;
+//        DoubleData rd = ((DoubleData)(input));
+//
+//        children[0].eval(state,thread,input,stack,individual,problem);
+//        result = rd.x;
+//
+//        children[1].eval(state,thread,input,stack,individual,problem);
+//        rd.x = result + rd.x;
+        }
+}
+
+
+
+
