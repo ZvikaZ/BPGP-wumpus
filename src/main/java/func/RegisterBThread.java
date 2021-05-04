f7a9022c818cd777d5a7d3390d969ea9b4e1dde6
Zvika Haramaty <haramaty.zvika@gmail.com>
2021-05-04 17:09:19 +0300

    now the generated code is legal :) (even though, with wrong events...)

---
+++ src/main/java/func/RegisterBThread.java
@@ -0,0 +1,38 @@
+package func;
+
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
+
+public class RegisterBThread extends GPNode { // implements EvalPrint {
+    public String toString() { return "RegisterBThread"; }
+
+    public void eval(final EvolutionState state,
+                     final int thread,
+                     final GPData input,
+                     final ADFStack stack,
+                     final GPIndividual individual,
+                     final Problem problem)
+        {
+            //TODO have different names?
+            String result = "bp.registerBThread(\"GeneratedBT\" , function() { \n" +
+                    "\twhile(true) {\n";
+
+            StringData rd = ((StringData)(input));
+
+            children[0].eval(state,thread,input,stack,individual,problem);
+            result += rd.str;
+
+            result += "\t}\n" +
+                    "});\n";
+
+            rd.str = result;
+        }
+}
+
+
+
+
