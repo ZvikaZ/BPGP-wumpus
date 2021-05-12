ae767d95ef30f796c9ad43478957cd802370713a
Zvika Haramaty <haramaty.zvika@gmail.com>
2021-05-12 13:07:49 +0300

    MyColor from ERC to regular GPNode ; readme changes; increased genome

--- readme.md
+++ readme.md
@@ -3,17 +3,20 @@ TODO
 
 NEXT
 ----
-- column +-1
+- why is the population getting smaller and smaller? (from 10 to 2)
+- debug: generation of 1, no crossover, mutation prob 0, see fitness makes sense
+- self play ; play against other individuals
 - graph:
   - population size
   - mean and avarage fitnesses
   - subplots - with one heading for all X and Y axis
-- debug: generation of 1, no crossover, mutation prob 0, see fitness makes sense    
-- self play ; play against other individuals
-- single figure at end of interactive run
+- column +-1 (in 'column_with_delta' branch)
 - improve grammar (diagonal?, specific cell?, arrays?)
 - bpjs verification?
 - sensible initialization
+- debug messages:
+  'Index 0 out of bounds for length 0' messages (related to threading??)
+  'Cannot assign field "evaluated" because the return value of "java.util.ArrayList.get(int)" is null'
 
 
 LATER
--- src/main/java/func/MyColor.java
+++ src/main/java/func/MyColor.java
@@ -2,72 +2,26 @@ package func;
 
 import ec.EvolutionState;
 import ec.Problem;
-import ec.gp.*;
-import ec.util.Code;
-import ec.util.DecodeReturn;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
+public class MyColor extends GPNode {
+    public String toString() { return "MyColor"; }
 
-public class MyColor extends ERC {
-    // true -> Yellow
-    public Boolean value;
+    public int expectedChildren() { return 0; }
 
-    public String name() {
-        return "MyColor";
-    }
+    public void eval(final EvolutionState state,
+                     final int thread,
+                     final GPData input,
+                     final ADFStack stack,
+                     final GPIndividual individual,
+                     final Problem problem)
+    {
 
-    public String toStringForHumans() {
-        if (value)
-            return "\"Yellow\"";
-        else
-            return "\"Red\"";
-    }
-
-    public String encode() {
-        return Code.encode(value);
-    }
-
-    public boolean decode(DecodeReturn dret) {
-        int pos = dret.pos;
-        String data = dret.data;
-        Code.decode(dret);
-        if (dret.type != DecodeReturn.T_BOOLEAN) {
-            // uh oh! Restore and signal error.
-            dret.data = data;
-            dret.pos = pos;
-            return false;
-        }
-        value = (dret.l != 0);
-        return true;
-    }
-
-    public boolean nodeEquals(GPNode node) {
-        return (node.getClass() == this.getClass() && ((MyColor)node).value == value);
-    }
-
-    public void readNode(EvolutionState state, DataInput dataInput) throws IOException {
-        value = dataInput.readBoolean();
-    }
-
-    public void writeNode(EvolutionState state, DataOutput dataOutput) throws IOException {
-        dataOutput.writeBoolean(value);
-    }
-
-    public void resetNode(EvolutionState state, int thread) {
-        //TODO support playing both colors ; currently always plays Yellow
-        value = true;
-    }
-
-    public void mutateNode(EvolutionState state, int thread) {
-        throw new RuntimeException();
-    }
-
-    public void eval(EvolutionState state, int thread, GPData input, ADFStack stack,
-                     GPIndividual individual, Problem Problem) {
         StringData rd = ((StringData)(input));
-        rd.str = toStringForHumans();
+        //TODO support playing the other side, and return "\"Red\""
+        rd.str = "\"Yellow\"";
     }
 }
-
