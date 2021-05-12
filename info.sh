cd2cad00845a91ebe7f9aa68b2790f035cbde6ec
Zvika Haramaty <haramaty.zvika@gmail.com>
2021-05-12 20:27:17 +0300

    info.sh: print rival

--- info.sh
+++ info.sh
@@ -3,3 +3,4 @@ git rev-parse HEAD
 grep generations src/main/resources/*params | grep -v '#'
 grep '\.size' src/main/resources/*params  | grep subpop
 grep numOfRandomRuns src/main/java/*Problem.java | grep final
+grep 'static Rival rival' src/main/java/*Problem.java
