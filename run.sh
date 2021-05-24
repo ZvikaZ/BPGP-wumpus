8b894c586e8079d706a5af0d93f68889b7de8e95
Zvika Haramaty <haramaty.zvika@gmail.com>
2021-05-24 15:45:49 +0300

    ecj: updated to 27.1-zvika-3 - local compilation
    fixes issues #75, #77
    with git master, and PRs #78, #79

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@
 #SBATCH --cpus-per-task=8 # 8 cpus per task - use for multithreading, usually with --tasks=1
 #SBATCH --tasks=1 # 1 processes - use for multiprocessing
 
-time $JAVA_HOME/bin/java -Dfile.encoding=UTF-8 -classpath target/classes:local-maven-repo/edu/gmu/cs/ecj/27/ecj-27.jar:$HOME/.m2/repository/edu/gmu/cs/ecj/27/ecj-27.jar:$HOME/.m2/repository/com/github/bthink-bgu/BPjs/0.12.0/BPjs-0.12.0.jar:$HOME/.m2/repository/org/mozilla/rhino/1.7.13/rhino-1.7.13.jar:$HOME/.m2/repository/commons-io/commons-io/2.5/commons-io-2.5.jar BpgpEvolve
+time $JAVA_HOME/bin/java -Dfile.encoding=UTF-8 -classpath target/classes:local-maven-repo/edu/gmu/cs/ecj/27.1-zvika-3/ecj-27.1-zvika-3.jar:$HOME/.m2/repository/edu/gmu/cs/ecj/27/ecj-27.jar:$HOME/.m2/repository/com/github/bthink-bgu/BPjs/0.12.0/BPjs-0.12.0.jar:$HOME/.m2/repository/org/mozilla/rhino/1.7.13/rhino-1.7.13.jar:$HOME/.m2/repository/commons-io/commons-io/2.5/commons-io-2.5.jar BpgpEvolve
 
