6c08eeab592ead1f39e553552170273452eed506
Zvika Haramaty <haramaty.zvika@gmail.com>
2021-10-28 15:19:41 +0300

    increased inds, etc. , according to Achiya's numbers

--- src/main/java/BpgpProblem.java
+++ src/main/java/BpgpProblem.java
@@ -15,7 +15,7 @@ import org.mozilla.javascript.NativeObject;
 
 public class BpgpProblem extends GPProblem implements SimpleProblemForm {
     static final boolean debug = false;
-    static final int numOfBoards = 5;
+    static final int numOfBoards = 70;      //TODO use split point
 
     protected double bpRun(String generatedCode, int boardNum) {
         generatedCode = "bp.log.setLevel(\"Warn\");\n" + generatedCode;   //TODO return
