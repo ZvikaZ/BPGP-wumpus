3048718aa5f8826e703fc031d0e36afefaa93c00
Zvika Haramaty <haramaty.zvika@gmail.com>
2021-11-01 22:01:49 +0200

    golden_fitness.py: more runs

--- src/main/python/golden_fitness.py
+++ src/main/python/golden_fitness.py
@@ -4,17 +4,22 @@
 import subprocess
 from pathlib import Path
 
+ROUNDS = 10
 NUM_OF_BOARDS = 100
 GOLDEN_JS = "golden.js"
 
 total = 0
-for i in range(1, NUM_OF_BOARDS+1):
-    p = subprocess.run(f'mvn exec:java@bpRun -Dexec.args="{GOLDEN_JS} {i}"', shell=True, capture_output=True)
-    output = p.stdout.decode().strip()
-    result = float(output.split('\n')[-1])
-    print(f"{i}: {result}")
-    total += result
+runs = 0
+for k in range(ROUNDS):
+    for i in range(1, NUM_OF_BOARDS+1):
+        runs += 1
+        p = subprocess.run(f'mvn exec:java@bpRun -Dexec.args="{GOLDEN_JS} {i}"', shell=True, capture_output=True)
+        output = p.stdout.decode().strip()
+        result = float(output.split('\n')[-1])
+        total += result
+        print(f"{k}, {i}: {result}, fitness so far: {total / runs}")
 
-avg = total / NUM_OF_BOARDS
+avg = total / runs
 print("=============")
-print("Fitness is: " + avg)
+print(avg)
+print("Fitness is: ", avg)
